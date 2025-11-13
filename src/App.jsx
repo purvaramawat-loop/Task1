a7fe5187b0fdfd43b99688261f5d4364dccae897
purvaramawat <purvaramawat@gmail.com>
2025-11-13 23:55:20 +0530

    Ready for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import React, { useState } from 'react'
 import File from "./Components/File.jsx";
+
 
 const App = () => {
 
   return (
-    <>
-        <File />
-    </>
+      
+    <HashRouter>
+       <Routes>
+            <Route path="/" element={<File/>} />
+
+       </Routes>
+    </HashRouter>
+    
   )
 }
 
